e9e6f25e2775ceade711927489e08aab07287e80
Harysson Soares <haryssonsoares@gmail.com>
2021-06-28 18:03:51 -0300

    wip: Lesson two, 40

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,8 @@
 import express from 'express'
+import 'reflect-metadata'
+import './database'
 
 const app = express()
 const port = 3000
-
-
-/**
- * GET      => Buscar uma informação
- * POST     => Inserir (Criar) uma informação
- * PUT      => Alterar uma informação
- * DELETE   => Remover um dado
- * PATCH    => Alterar uma informação específica
- */
-
-app.get("/test", (req, res) => {
-    // req => input
-    // res => output
-    res.send("Olá NLW")
-})
-
-app.post("/test-post", (req, res) => {
-    // req => input
-    // res => output
-    res.send("Olá NLW Rota post")
-})
 
 app.listen(port, () => {console.log(`Server running on ${port}`)})
