0aa6cf70346e5ab1398b2307a78b90ed5493cbff
Harysson Soares <haryssonsoares@gmail.com>
2021-07-02 18:03:07 -0300

    lesson 4: hashed password to user service

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,25 +1,34 @@
 import { getCustomRepository } from "typeorm"; 
 import { UsersRepositories } from "../repositories/UsersRepositories"
+import { hash } from "bcryptjs"
 
 interface IUserRequest {
   name: string
   email: string
+  password: string
   admin?: boolean
 }
 
 class CreateUserService {
   
-  async execute({name, email, admin} : IUserRequest) {
+  async execute({name, email, admin, password} : IUserRequest) {
     const usersRepository = getCustomRepository(UsersRepositories);
 
     const userAlreadyExists = await usersRepository.findOne({
       email
     })
 
+    if(userAlreadyExists) {
+      throw new Error("User already exists")
+    }
+
+    const passwordHash = await hash(password, 8)
+
     const user = usersRepository.create({
       name,
       email,
-      admin
+      admin,
+      password: passwordHash
     })
 
     await usersRepository.save(user);
