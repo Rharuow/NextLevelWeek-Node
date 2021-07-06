fb238784fc97c363db935e5f7895907087dd69bb
Harysson Soares <haryssonsoares@gmail.com>
2021-07-06 18:32:52 -0300

    feat: manipulated req to recovery user id, 38

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -3,6 +3,10 @@ import {Request, Response, NextFunction} from 'express'
 
 export function ensureAdmin(req: Request, res: Response, next: NextFunction) {
 
+  const { user_id } = req
+
+  console.log(user_id)
+
   const admin = true;
 
   if(admin) {
