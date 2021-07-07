f17eb1ff8dc6ad584d4c7025d1ed3f1ad868b4ea
Harysson Soares <haryssonsoares@gmail.com>
2021-07-07 14:10:10 -0300

    improvements: cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction} from 'express'
 import "express-async-errors"
 import 'reflect-metadata'
+import cors from 'cors'
 import './database'
 import { router } from './router'
 
@@ -8,6 +9,7 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(cors())
 
 app.use(router)
 
